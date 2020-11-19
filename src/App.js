35c9a79e529a0ad8309f638d5019741836803d70
Dailey Kaze <daileykaze@gmail.com>
2020-11-18 19:52:12 -0500

    Added css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import axios from 'axios'
+import './components/style.css'
 
 function App() {
   const [users, setUsers] = useState([])
@@ -11,36 +12,38 @@ function App() {
   }, [])
 
   async function getRandomUsers(){
-    const result = await axios.get('https://randomuser.me/api/?results=50')
+    const result = await axios.get('https://randomuser.me/api/?results=25')
     setUsers(result.data.results)
   }
 
   return (
     <div className="App">
       <Navbar />
-      <table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => 
-           <tr>
-            <td>
-              <img src={user.picture.thumbnail} alt="profile"/>
-            </td>
-            <td>{user.name.first} {user.name.first}</td>
-            <td>{user.phone}</td>
-            <td>{user.email}</td>
-          </tr>
-          )}
-          
-        </tbody>
-      </table>
+      <div className="container" style={{marginTop: "20px", marginBottom: "20px"}}>
+        <table style={{width: "100%"}}>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => 
+            <tr>
+              <td>
+                <img src={user.picture.thumbnail} alt="profile"/>
+              </td>
+              <td>{user.name.first} {user.name.last}</td>
+              <td>{user.phone}</td>
+              <td>{user.email}</td>
+            </tr>
+            )}
+            
+          </tbody>
+        </table>
+      </div>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './style.css'
 
 function Footer(){
     return (
         <footer>
-            <ul>
+            <ul className="footer">
                 <li>CopyRight 2020</li>
                 <li>Users by RandomUsers.me</li>
             </ul>
